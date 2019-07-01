a9745f9783fcdc6118666186f742fa29087b1e26
Jose Villa <jose.villa@digipen.edu>
2019-07-01 14:44:21 +0200

    Added the console to the code.
    -The editor has its own console.
    -Added the menu "Tools"
    -Added the button "Console" in "Tools" to activate/deactivate the console window
    -Created the stdJ namespace, it will contain editor related variables.
    -Added a global variable called "console" of type stringstream, which is seted in the Main.cpp

--- THE SAMPLE ENGINE/current/src/Demos/Main.cpp
+++ THE SAMPLE ENGINE/current/src/Demos/Main.cpp
@@ -4,9 +4,18 @@
 #include "JsonDemo\JsonDemo.h"
 #include "Imgui Demo\ImGuiDemo.h"
 
+std::stringstream console;
+
 void main(void)
 {
+
+	auto* coutbuf = std::cout.rdbuf();
+	auto old_buf = std::cout.rdbuf(console.rdbuf());
+
+
 	aexEngine->Initialize();
 	aexEngine->Run(new ImGuiDemo);
 	AEX::AEXEngine::ReleaseInstance();
+
+	std::cout.rdbuf(coutbuf);
 }
