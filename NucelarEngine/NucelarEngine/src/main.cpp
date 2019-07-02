b41369f0805bba782c5c5286a6e0e50643cad5a9
Javi Guerra <j.guerra@digipen.edu>
2019-07-02 12:16:33 +0200

    OpenGL linked and created window
    Orange window created. Needs to be organized.

---
+++ NucelarEngine/NucelarEngine/src/main.cpp
@@ -0,0 +1,28 @@
+#include "Graphics.h"
+#include <iostream>
+
+
+int main()
+{
+
+	
+	GLFWwindow* window = Graphics::InitializeWindow(800, 600);
+
+
+	
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(255.0f/255.0f, 164.0f/255.0f, 32.0f/255.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
