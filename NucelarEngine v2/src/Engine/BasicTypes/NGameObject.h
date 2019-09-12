72517fe57750029f32db583eb1edb1bee7d1e123
Ibai <ibaiabaunza@protonmail.com>
2019-09-12 22:54:19 +0200

    I put gameobject thing, really simple thing. It is on a folder called basic types, let's put there the comp too okay guys?
    Let's have some fun with engine architechture!

---
+++ NucelarEngine v2/src/Engine/BasicTypes/NGameObject.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "../AEX/AEXRtti.h"
+#include "../AEX/AEXBase.h"
+using namespace AEX;
+class  NGameObject : public IBase
+{
+	AEX_RTTI_DECL(NGameObject, IBase);
+
+public:
+	NGameObject();
+	virtual ~NGameObject();
+
+	virtual void SetEnabled(bool enabled); //Component sleep
+	virtual void Intialize(); //initializes every component
+	virtual void ShutDown(); //frees stuff
+
+	#pragma region// COMPONENT MANAGEMENT
+	//Getters, decoment when component system gets created
+
+	//u32 GetCompCount() const;
+	//IComp * GetComp(u32 index)const;
+	//IComp * GetComp(const char * type)const;
+	//IComp * GetComp(const Rtti & type)const;
+	//IComp * GetCompName(const char * compName, const char *compType = NULL)const;
+	void OnGui(); // I guess this is when it is on the scene
+	template<class T>
+	T* GetComp(const char * name = NULL); //returns a component in the vector with this name
+
+	template<class T>
+	T* GetCompDerived(const char * name = NULL);
+	template<class T>
+	T* NewComp(const char * name = NULL);
+
+	//Add and remove comps with pointer 
+	//IComp * AddComp(Icomp * pComp);
+	//void RemoveComp(IComp pComp);
+
+	//Remove Components with name and type (1st encounter)
+	void RemoveCompType(const char * compType);
+	void RemoveCompType(const Rtti & compType);
+	void RemoveCompName(const char * compName, const char * compType = NULL);
+
+	//Remove comps
+	void RemoveAllComp();
+
+	// debug only!!
+	//std::vector<IComp*> &GetComps() { return mComps; }
+	//const std::vector<IComp*> &GetComps() const { return mComps; }
+protected:
+	//This comment would have the vector of comps
+	bool mEnabled;
+};
+
+
