8cc5f6412208b08c310756d0db9f5deb5533b3cc
Ibai <ibaiabaunza@protonmail.com>
2019-09-15 11:40:38 +0200

    GameObject and Component class
    Hello there people, here go again. The component is there and gameobject too, with their implementations. Not the individual component systems though. Have fun programming okay

--- NucelarEngine v2/src/Engine/BasicTypes/NGameObject.h
+++ NucelarEngine v2/src/Engine/BasicTypes/NGameObject.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "../AEX/AEXRtti.h"
 #include "../AEX/AEXBase.h"
+#include "NComponent.h"
 using namespace AEX;
 class  NGameObject : public IBase
 {
@@ -17,38 +18,45 @@ public:
 	#pragma region// COMPONENT MANAGEMENT
 	//Getters, decoment when component system gets created
 
-	//u32 GetCompCount() const;
-	//IComp * GetComp(u32 index)const;
-	//IComp * GetComp(const char * type)const;
-	//IComp * GetComp(const Rtti & type)const;
-	//IComp * GetCompName(const char * compName, const char *compType = NULL)const;
+	u32 GetCompCount() const;
+	IComp * GetComp(u32 index)const;
+	IComp * GetComp(const char * type)const;
+	IComp * GetComp(const Rtti & type)const;
+	IComp * GetCompName(const char * compName, const char *compType = NULL)const;
 	void OnGui(); // I guess this is when it is on the scene
 	template<class T>
-	T* GetComp(const char * name = NULL); //returns a component in the vector with this name
+	T* GetComp(const char * compName = NULL); //returns a component in the vector with this name
 
 	template<class T>
-	T* GetCompDerived(const char * name = NULL);
+	T* GetCompDerived(const char * compName = NULL);
 	template<class T>
-	T* NewComp(const char * name = NULL);
+	T* NewComp(const char * compName = NULL);
 
 	//Add and remove comps with pointer 
-	//IComp * AddComp(Icomp * pComp);
-	//void RemoveComp(IComp pComp);
+	IComp * AddComp(IComp * pComp);
+	void RemoveComp(IComp * pComp);
 
 	//Remove Components with name and type (1st encounter)
 	void RemoveCompType(const char * compType);
 	void RemoveCompType(const Rtti & compType);
 	void RemoveCompName(const char * compName, const char * compType = NULL);
 
+	void RemoveAllCompType(const char * compType);
+	void RemoveAllCompType(const Rtti & compType);
+	void RemoveAllCompName(const char * compName, const char * compType = NULL);
+
+	
+	
 	//Remove comps
 	void RemoveAllComp();
 
 	// debug only!!
-	//std::vector<IComp*> &GetComps() { return mComps; }
-	//const std::vector<IComp*> &GetComps() const { return mComps; }
+	std::vector<IComp*> &GetComps() { return mComps; }
+	const std::vector<IComp*> &GetComps() const { return mComps; }
 protected:
 	//This comment would have the vector of comps
-	bool mEnabled;
+	std::vector<IComp*> mComps;
+	bool mbEnabled;
 };
 
 
